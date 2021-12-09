761b9f3787d9815cf6fc982b6fab4e84c66272ac
Borna Radojčić <br52543@fer.hr>
2021-12-10 00:30:39 +0100

    Ispravljene greške 2. labosa

--- JS/server.js
+++ JS/server.js
@@ -34,56 +34,28 @@ app.use(express.urlencoded({
    extended: true
 }));
 app.use('/views', express.static('views'))
-app.get('/', function (req, res, next) {
-  res.render('index', {
-  });
-});
-
-app.get('/db', async function (req, res, next) {
+app.get('/', async function (req, res, next) {
   const sql = 'SELECT * FROM klub natural join igrac natural join trener';
   try {
       const data = (await db.query(sql, [])).rows;
       fs.writeFileSync("views/kosarkaski_klubovi.json", JSON.stringify(data))
       const csv = new ObjectsToCsv(data)
       await csv.toDisk('./views/kosarkaski_klubovi.csv')
-      res.render('db', { data: data });
+      res.render('index', {});
   } catch (err) {
       console.log(err);
   }
 });
 
-app.post('/db', async function (req, res, next) {
-  var name = req.body.fname;
-  var drop = req.body.dropdown;
-  var isnum = /^\d+$/.test(name);
-  try{
-      if (drop == "svi" && !isnum) {
-          var sql = 'SELECT * FROM klub natural join igrac natural join trener where (imeklub=$1 or arenaklub=$1 or savdrzava=$1  or imeigrac=$1 or prezimeigrac=$1  or pozicijaigrac=$1 or uni_hs_club=$1 or imetrener=$1 or prezimetrener=$1)';
-      }
-      else if (drop == "svi" && isnum) {
-          var sql = 'SELECT * FROM klub natural join igrac natural join trener where (idklub=$1 or godosnutka=$1 or brojdresa=$1)';
-
-      }
-      else {
-          var sql = format('SELECT * FROM klub natural join igrac natural join trener where %I=$1', drop);
-      }
-
-
-      const data = (await db.query(sql, [name])).rows;
-     // var x=JSON.stringify(data);
-      //console.log(x);
-      fs.writeFileSync( "views/kosarkaski_klubovi.json", JSON.stringify(data))
-      const csv = new ObjectsToCsv(data)
-      await csv.toDisk('./views/kosarkaski_klubovi.csv')
-      res.render('db',{data:data});}
-      catch(e){
-        console.log("ERROR"+e);
-      }
-      
+app.get('/db', async function (req, res, next) {
+  const sql = 'SELECT * FROM klub natural join igrac natural join trener';
+  try {
+      const data = (await db.query(sql, [])).rows;
+      res.render('db', { data: data });
+  } catch (err) {
+      console.log(err);
+  }
 });
-
-
-
 
 
 //pokretanje poslužitelja na portu 3000
